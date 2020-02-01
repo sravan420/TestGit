959a7e07e76db41a75cc3cf57930e4dbeeb44041
sravan784 <sravan.java784@gmail.com>
2020-02-01 16:03:52 -0600

    first message

---
+++ DemoGui/src/main/java/com/example/demo/DemoGuiApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGuiApplication.class, args);
+		//added comments
+		
+		
+	}
+
+}
+
---
+++ DemoGui/src/main/java/com/example/demo/Test.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+
+public class Test {
+
+	//added test class
+}
